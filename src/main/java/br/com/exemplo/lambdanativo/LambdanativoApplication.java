d9ef3bf11faa69f5e1c8ca485cf2f039e06f27bb
kaiqueberaguas <ksakX@outlook.com>
2024-03-18 11:05:13 -0300

    Commit inicial

---
+++ src/main/java/br/com/exemplo/lambdanativo/LambdanativoApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.lambdanativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdanativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LambdanativoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/exemplo/lambdanativo/RequestHandler.java
@@ -0,0 +1,5 @@
+package br.com.exemplo.lambdanativo;
+
+public class RequestHandler {
+
+}
